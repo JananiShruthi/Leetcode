db80a380778226365deb47a297d6f96f315f5469
JananiShruthi <111137067+JananiShruthi@users.noreply.github.com>
2026-02-26 11:32:10 +0530

    Updated the full solution

--- leetcode_1356.py
+++ leetcode_1356.py
@@ -1,15 +1,16 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
-        d = {}
+        d = []
         for i in arr:
             num = i
-            d[i] = 0
+            d.append([i, 0])
             while num >= 1:
                 if num%2 == 1:
-                    d[i] += 1
+                    d[-1][1] += 1
                 num = num // 2
-        sorted_dict = dict(sorted(d.items(), key=lambda item: item[1]))
-        print(f"Sorted dict: {sorted_dict}")
-        ans = [key for key in sorted_dict.keys()]
-        print(ans)
-        return ans
+
+        print(d)
+        sorted_list = sorted(d, key=lambda item: (item[1], item[0]))
+        print(f"sorted_list: {sorted_list}")
+        
+        return [x[0] for x in sorted_list]
