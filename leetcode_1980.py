c489f2a01eb9b02c72fc2bce1d3dd61be032c956
JananiShruthi <111137067+JananiShruthi@users.noreply.github.com>
2026-03-08 12:59:02 +0530

    chatGPT solution (much simpler) attached too

--- leetcode_1980.py
+++ leetcode_1980.py
@@ -27,3 +27,14 @@ class Solution:
             for i in range(num_bits - len(ans)):
                 ans = "0" + ans
             return ans
+
+# ChatGPT optimal solution:
+# class Solution:
+#     def findDifferentBinaryString(self, nums: List[str]) -> str:
+#         ans = ""
+#         for i in range(len(nums)):
+#             if nums[i][i] == "0":
+#                 ans += "1"
+#             else:
+#                 ans += "0"
+#         return ans
